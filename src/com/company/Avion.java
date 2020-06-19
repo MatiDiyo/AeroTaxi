edf25dd54ab9ca36a7f61d324affe08a22a65e60
Franco <francoperaltagg@gmail.com>
2020-06-19 09:07:44 -0300

    Getter & Setter (Avion)

--- src/com/company/Avion.java
+++ src/com/company/Avion.java
@@ -21,6 +21,51 @@ public class Avion {
         this.tarifaFija = tarifaFija;
     }
 
-    //hacer commit del contructor
+    public short getCapacidadCombustible() {
+        return capacidadCombustible;
+    }
 
+    public void setCapacidadCombustible(short capacidadCombustible) {
+        this.capacidadCombustible = capacidadCombustible;
+    }
+
+    public double getCostoPorKm() {
+        return costoPorKm;
+    }
+
+    public void setCostoPorKm(double costoPorKm) {
+        this.costoPorKm = costoPorKm;
+    }
+
+    public short getCapacidadMaximaPasajeros() {
+        return capacidadMaximaPasajeros;
+    }
+
+    public void setCapacidadMaximaPasajeros(short capacidadMaximaPasajeros) {
+        this.capacidadMaximaPasajeros = capacidadMaximaPasajeros;
+    }
+
+    public short getVelocidadMaxima() {
+        return velocidadMaxima;
+    }
+
+    public void setVelocidadMaxima(short velocidadMaxima) {
+        this.velocidadMaxima = velocidadMaxima;
+    }
+
+    public Propulsion getPropulsion() {
+        return propulsion;
+    }
+
+    public void setPropulsion(Propulsion propulsion) {
+        this.propulsion = propulsion;
+    }
+
+    public int getTarifaFija() {
+        return tarifaFija;
+    }
+
+    public void setTarifaFija(int tarifaFija) {
+        this.tarifaFija = tarifaFija;
+    }
 }
