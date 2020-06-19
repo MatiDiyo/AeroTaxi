003a01d31bd3180365decd56ba98151a18f738f7
Franco <francoperaltagg@gmail.com>
2020-06-19 09:04:34 -0300

    En avion pasamos de modificadores de acceso protected a private y constructores

--- src/com/company/Avion.java
+++ src/com/company/Avion.java
@@ -2,15 +2,25 @@ package com.company;
 
 public class Avion {
 
-    protected short capacidadCombustible;
-    protected double costoPorKm;
-    protected short capacidadMaximaPasajeros;
-    protected short velocidadMaxima;
-    protected Propulsion propulsion;
-    protected boolean servicioCatering;
-    protected int tarifaFija;
+    private short capacidadCombustible;
+    private double costoPorKm;
+    private short capacidadMaximaPasajeros;
+    private short velocidadMaxima;
+    private Propulsion propulsion;
+    //private boolean servicioCatering; usaremos una interface para el catering
+    private int tarifaFija;
 
+    public Avion(){}
 
+    public Avion(short capacidadCombustible, double costoPorKm, short capacidadMaximaPasajeros, short velocidadMaxima, Propulsion propulsion, int tarifaFija) {
+        this.capacidadCombustible = capacidadCombustible;
+        this.costoPorKm = costoPorKm;
+        this.capacidadMaximaPasajeros = capacidadMaximaPasajeros;
+        this.velocidadMaxima = velocidadMaxima;
+        this.propulsion = propulsion;
+        this.tarifaFija = tarifaFija;
+    }
 
+    //hacer commit del contructor
 
 }
