646e87c80b5bec2f32e967b6719b12b9cbe98a5f
Franco <francoperaltagg@gmail.com>
2020-06-20 12:43:18 -0300

    Se agregaron equals y hashcode a aviones

--- src/com/company/AvionBronze.java
+++ src/com/company/AvionBronze.java
@@ -18,6 +18,16 @@ public class AvionBronze extends Avion {
     }
 
     @Override
+    public boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
     public String toString() {
         return "Avion - Bronze" +
                 "\n\t-Id: " + getUuid() + "." +
