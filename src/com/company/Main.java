e018ab2671766aa2ccec49c58e3cfffd60b0a409
MatiDiyo <45548452+MatiDiyo@users.noreply.github.com>
2020-06-20 20:43:04 -0300

    Revert "Borrando librerías del Main"
    This reverts commit 2e50ce63b8fc966ec4906b75b45b6e7d681c0f46.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,14 @@
 package com.company;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import netscape.javascript.JSObject;
+
+import java.io.*;
+import java.nio.file.attribute.UserDefinedFileAttributeView;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main
 {
     public static void main(String[] args)
