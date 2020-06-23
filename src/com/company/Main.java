04f3fefb78fa3a002707abbbc7ba50d141d77c8d
MatiDiyo <45548452+MatiDiyo@users.noreply.github.com>
2020-06-23 17:11:27 -0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,14 +18,7 @@ public class Main
         Archivo archivoAviones = new Archivo("archivoAviones.json");
         Archivo archivoVuelo = new Archivo("archivoVuelos.json");
 
-        //Usuario user = new Usuario("Alberto", "Fernandez", "10344456",(byte)61, 0);
-
-        //archivoUsuarios.agregarElemento(user, Usuario.class);
-
-        //archivoUsuarios.createArchivoUsuarios();
-        //archivoFunciones.createArchivoAviones();
-
-        /*ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
+        ArrayList<Usuario> listaUsuarios = new ArrayList<Usuario>();
         ArrayList<Avion> listaAviones = new ArrayList<Avion>();
         ArrayList<Vuelo> listaVuelos = new ArrayList<>();
 
@@ -33,6 +26,6 @@ public class Main
         listaAviones = archivoAviones.archivoToArrayAvion();
         listaVuelos = archivoVuelo.archivoToArrayVuelos();
 
-        Sistema.cargarSistema(listaUsuarios , listaAviones , listaVuelos);*/
+        Sistema.cargarSistema(listaUsuarios , listaAviones , listaVuelos);
     }
 }
